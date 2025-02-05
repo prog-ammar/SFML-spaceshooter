e95cf2a0b7af95122610a349656d0ebcd7fb0b00
prog-ammar <ammarofc2005@gmail.com>
2025-02-06 01:49:09 +0500

    Fixed Ship Movement

--- code.cpp
+++ code.cpp
@@ -12,15 +12,16 @@ class Game
     Sprite backg;
     Texture spaceship;
     Sprite spaces;
+    int x,y,move;
     public:
-    Game(): window(VideoMode(1280,720),"SPACE-SHOOTER")
+    Game(): window(VideoMode(800,800),"SPACE-SHOOTER"),x(650),y(320),move(10)
     {
        background.loadFromFile("1.jpg");
        backg.setTexture(background);
        spaceship.loadFromFile("ship.PNG");
        spaces.setTexture(spaceship);
        spaces.setScale(0.15,0.15);
-       spaces.setPosition(550,550);
+       spaces.setPosition(y,x);
     }
     void run()
     {
@@ -36,9 +37,38 @@ class Game
             }
             window.clear(Color::White);
             window.draw(backg);
+            spaceship_movement();
             window.draw(spaces);
             window.display();
         }
+    }
+    void spaceship_movement()
+    {
+        if(Keyboard::isKeyPressed(Keyboard::W))
+        {
+            x=x-move;
+            spaces.setPosition(y,x);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::S))
+        {
+            x=x+move;
+            spaces.setPosition(y,x);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::A))
+        {
+           y=y-move;
+           spaces.setPosition(y,x);
+        }
+        if(Keyboard::isKeyPressed(Keyboard::D))
+        {
+            y=y+move;
+           spaces.setPosition(y,x);
+        }
+        if(x>650)x=650;
+        if(y>680)y=680;
+        if(x<0)x=0;
+        if(y<0)y=0;
+        spaces.setPosition(y,x);
     }
 };
 
