a52477e4dc8cca45e268f603ed111027443876dc
prog-ammar <ammarofc2005@gmail.com>
2025-02-06 03:01:09 +0500

    Idk What the Actual Fuck im doing

--- code.cpp
+++ code.cpp
@@ -12,6 +12,8 @@ class Game
     Sprite backg;
     Texture spaceship;
     Sprite spaces;
+    Texture asteriod;
+    Sprite aster;
     int x,y,move;
     public:
     Game(): window(VideoMode(800,800),"SPACE-SHOOTER"),x(650),y(320),move(10)
@@ -20,13 +22,29 @@ class Game
        backg.setTexture(background);
        spaceship.loadFromFile("ship.PNG");
        spaces.setTexture(spaceship);
+       asteriod.loadFromFile("asteroid.PNG");
+       aster.setTexture(asteriod);
+       aster.setScale(0.10,0.10);
        spaces.setScale(0.15,0.15);
        spaces.setPosition(y,x);
     }
     void run()
     {
+        Clock clock;
+        Time timeSince=Time::Zero;
+        Time TimePerFrame=seconds(1.f/60.f);
         while(window.isOpen())
         {
+            timeSince+=clock.restart();
+            while (timeSince>TimePerFrame)
+            {
+                timeSince-=TimePerFrame;
+                window.clear(Color::White);
+                window.draw(backg);
+                spaceship_movement(timeSince);
+                window.draw(spaces);
+            }
+            
             Event ef;
             while(window.pollEvent(ef))
             {
@@ -35,40 +53,38 @@ class Game
                     window.close();
                 }
             }
-            window.clear(Color::White);
-            window.draw(backg);
-            spaceship_movement();
-            window.draw(spaces);
+            window.draw(aster);
             window.display();
         }
     }
-    void spaceship_movement()
+    void spaceship_movement(Time d)
     {
+        Vector2f movement(0,0);
         if(Keyboard::isKeyPressed(Keyboard::W))
         {
-            x=x-move;
-            spaces.setPosition(y,x);
+            movement.y-=move;
         }
         if(Keyboard::isKeyPressed(Keyboard::S))
         {
-            x=x+move;
-            spaces.setPosition(y,x);
+            movement.y+=move;
         }
         if(Keyboard::isKeyPressed(Keyboard::A))
         {
-           y=y-move;
-           spaces.setPosition(y,x);
+           movement.x-=move;
         }
         if(Keyboard::isKeyPressed(Keyboard::D))
         {
-            y=y+move;
-           spaces.setPosition(y,x);
+            movement.x+=move;
         }
         if(x>650)x=650;
         if(y>680)y=680;
         if(x<0)x=0;
         if(y<0)y=0;
-        spaces.setPosition(y,x);
+        spaces.move(movement);
+    }
+    void asteriod_falling()
+    {
+
     }
 };
 
